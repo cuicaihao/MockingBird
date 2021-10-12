ea0b8622d558ef2b6794c3296fdbf1febe2859c4
Chris (Caihao) Cui <cuicaihao@users.noreply.github.com>
2021-10-12 12:08:23 +1100

    test eng and chs

--- synthesizer/utils/symbols.py
+++ synthesizer/utils/symbols.py
@@ -8,10 +8,13 @@ through Unidecode. For other data, you can modify _characters. See TRAINING_DATA
 
 _pad        = "_"
 _eos        = "~"
-# _characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!\'(),-.:;? ' 
-_characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz12340!\'(),-.:;? ' # use this old one if you want to train old model 
+# _characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!\'(),-.:;? ' # 75 This line is used for Chinese.
+# _characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz12340!\'(),-.:;? ' # 70 use this old one if you want to train old model 
 # Prepend "@" to ARPAbet symbols to ensure uniqueness (some are the same as uppercase letters):
 #_arpabet = ["@' + s for s in cmudict.valid_symbols]
+
+_characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!\'\"(),-.:;? " # 66 This line is need for English generation
+
 
 # Export all symbols:
 symbols = [_pad, _eos] + list(_characters) #+ _arpabet
